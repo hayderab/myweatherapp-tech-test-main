0b5e10c695a75436178f766bb362421106851f3f
hayderab <haider.1999@outlook.com>
2023-05-25 16:17:48 +0100

    exception handling

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -6,6 +6,7 @@ import com.weatherapp.myweatherapp.service.WeatherService;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,15 +26,28 @@ public class WeatherController {
 
   @GetMapping("/compare/daylight/{city1}/{city2}")
   public ResponseEntity<String> compareDaylight(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
-    String response = weatherService.compareDaylight(city1, city2);
-    return ResponseEntity.ok(response);
+    try {
+      String response = weatherService.compareDaylight(city1, city2);
+      return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      e.printStackTrace();
+      // Handle the exception and return an error response
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while comparing daylight. Double check the city name.");
+    }
   }
 
   @GetMapping("/check/rain/{city1}/{city2}")
   public ResponseEntity<String> checkRain(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
+    try {
       String response = weatherService.checkRain(city1, city2);
       return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      e.printStackTrace();
+      // Handle the exception and return an error response
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred while checking rain conditions. Double check the city name.");
+    }
   }
+
 
 
 }
