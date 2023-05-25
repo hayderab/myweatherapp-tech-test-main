5b800876e41de101df437ec9b027bf5570d31866
hayderab <haider.1999@outlook.com>
2023-05-25 16:08:49 +0100

    endpoints for checking daylight and rain

--- src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
+++ src/main/java/com/weatherapp/myweatherapp/controller/WeatherController.java
@@ -2,6 +2,9 @@ package com.weatherapp.myweatherapp.controller;
 
 import com.weatherapp.myweatherapp.model.CityInfo;
 import com.weatherapp.myweatherapp.service.WeatherService;
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,14 +19,21 @@ public class WeatherController {
 
   @GetMapping("/forecast/{city}")
   public ResponseEntity<CityInfo> forecastByCity(@PathVariable("city") String city) {
-
     CityInfo ci = weatherService.forecastByCity(city);
-
     return ResponseEntity.ok(ci);
   }
 
-  // TODO: given two city names, compare the length of the daylight hours and return the city with the longest day
+  @GetMapping("/compare/daylight/{city1}/{city2}")
+  public ResponseEntity<String> compareDaylight(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
+    String response = weatherService.compareDaylight(city1, city2);
+    return ResponseEntity.ok(response);
+  }
 
-  // TODO: given two city names, check which city its currently raining in
+  @GetMapping("/check/rain/{city1}/{city2}")
+  public ResponseEntity<String> checkRain(@PathVariable("city1") String city1, @PathVariable("city2") String city2) {
+      String response = weatherService.checkRain(city1, city2);
+      return ResponseEntity.ok(response);
+  }
+
 
 }
